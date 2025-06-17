ec3c98c2fe7a0c49373b27835dd39f6111cd19ec
jason.mcpeak@gmail.com <jason.mcpeak@gmail.com>
2025-06-16 20:46:31 -0500

    lots of improvements

--- src/components/list/drawer/new-chat/new-chat.tsx
+++ src/components/list/drawer/new-chat/new-chat.tsx
@@ -4,6 +4,7 @@ import { sxListItemButton, sxListItemIcon } from "@/components/button/consts";
 import KeyboardShortcut from "@/components/button/keyboard-shortcut";
 import useIsHovered from "@/hooks/use-is-hovered";
 import MapsUgcOutlined from "@mui/icons-material/MapsUgcOutlined";
+import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -12,12 +13,20 @@ export default function NewChat() {
 	const { visible, ref, ...props } = useIsHovered();
 
 	return (
-		<ListItemButton {...props} component="a" href="/" sx={sxListItemButton}>
-			<ListItemIcon sx={sxListItemIcon}>
-				<MapsUgcOutlined fontSize="small" />
-			</ListItemIcon>
-			<ListItemText primary="New chat" />
-			<KeyboardShortcut visible={visible}>⇧ ⌘ O</KeyboardShortcut>
-		</ListItemButton>
+		<ListItem disablePadding disableGutters>
+			<ListItemButton
+				{...props}
+				component="a"
+				disableGutters
+				href="/"
+				sx={sxListItemButton}
+			>
+				<ListItemIcon sx={sxListItemIcon}>
+					<MapsUgcOutlined fontSize="small" />
+				</ListItemIcon>
+				<ListItemText primary="New chat" />
+				<KeyboardShortcut visible={visible}>⇧ ⌘ O</KeyboardShortcut>
+			</ListItemButton>
+		</ListItem>
 	);
 }
--- src/components/list/drawer/search-chats/search-chats.tsx
+++ src/components/list/drawer/search-chats/search-chats.tsx
@@ -4,6 +4,7 @@ import { sxListItemButton, sxListItemIcon } from "@/components/button/consts";
 import KeyboardShortcut from "@/components/button/keyboard-shortcut";
 import useIsHovered from "@/hooks/use-is-hovered";
 import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -12,17 +13,20 @@ export default function SearchChats() {
 	const { visible, ref, ...props } = useIsHovered();
 
 	return (
-		<ListItemButton
-			{...props}
-			component="a"
-			href="/search"
-			sx={sxListItemButton}
-		>
-			<ListItemIcon sx={sxListItemIcon}>
-				<SearchOutlined fontSize="small" />
-			</ListItemIcon>
-			<ListItemText primary="Search chats" />
-			<KeyboardShortcut visible={visible}>⌘ K</KeyboardShortcut>
-		</ListItemButton>
+		<ListItem disablePadding disableGutters>
+			<ListItemButton
+				{...props}
+				component="a"
+				disableGutters
+				href="/search"
+				sx={sxListItemButton}
+			>
+				<ListItemIcon sx={sxListItemIcon}>
+					<SearchOutlined fontSize="small" />
+				</ListItemIcon>
+				<ListItemText primary="Search chats" />
+				<KeyboardShortcut visible={visible}>⌘ K</KeyboardShortcut>
+			</ListItemButton>
+		</ListItem>
 	);
 }
